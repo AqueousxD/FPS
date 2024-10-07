9cd431f7cbe70b0ab5c502c34e8ee2302b291014
Aqueous <45310273+AqueousxD@users.noreply.github.com>
2024-10-07 15:33:39 +0100

    added gun and primitive raycasting

---
+++ Assets/Scripts/WeaponUtility.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponUtility : MonoBehaviour
+{
+    [SerializeField] Camera playerCam;
+    [SerializeField] float raycastRange;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) Shoot();
+    }
+
+    void Shoot()
+    {
+        RaycastHit hitInfo;
+        Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hitInfo, raycastRange);
+        Debug.DrawRay(playerCam.transform.position, playerCam.transform.forward, Color.red, 2);
+
+        Debug.Log(hitInfo.transform.name);
+    }
+}
